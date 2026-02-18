5a2da13c2d26c0373e99471d54845ead5ec84a0f
rajadurai <rajadurai1981@gmail.com>
2026-02-18 19:22:41 +0530

    responding URL parameter

--- app.js
+++ app.js
@@ -37,6 +37,21 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
+//responding URL parameter
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  if (id > tours.length) //if (!tour)
+  {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid ID',
+    });
+  }
+  res.status(200).json({ status: 'success', data: { tour } });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`app running on port ${port}...`);
